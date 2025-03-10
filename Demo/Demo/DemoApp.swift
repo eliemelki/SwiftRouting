ff5f55c9e995d34f440f638ca6c717a7752a1243
Elias Melki <eli.melki@twgroup.co.nz>
2025-03-10 15:19:46 +1300

    Project Setup

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Elie Melki on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
